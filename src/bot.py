3f72174131456d0d2463eb7e3071ecf4e69e4ef1
Jolly Rabbit <jollyrabbitok@gmail.com>
2025-01-18 02:48:22 -0800

    Implement reply and store message logic

--- src/bot.py
+++ src/bot.py
@@ -3,16 +3,25 @@ import asyncio
 import os
 import requests
 import logging
+from dotenv import load_dotenv
+
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 from api_client import MessageAPIClient
+from parser import Parser
+from states import BotStates
+
+load_dotenv()
 
 # Configure logging
 MY_BOT_NAME='wishengine'
+log_level = os.getenv('LOG_LEVEL', 'INFO')
+DEBUG=(log_level.upper() == 'DEBUG')
+numeric_level = getattr(logging, log_level.upper(), logging.INFO)
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=numeric_level
 )
 logger = logging.getLogger(MY_BOT_NAME)
 
@@ -66,20 +75,63 @@ async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def fetch(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Show last messages in server when the command /fetch is issued."""
-    messages = api.get_messages({'limit': 2})
-    if not messages:
+    messages_yes = api.get_messages({'limit': 1, 'state': BotStates.POSITIVE.value})
+    messages_no  = api.get_messages({'limit': 1, 'state': BotStates.NEGATIVE.value})
+    messages_list = [(BotStates.POSITIVE.value, messages_yes),
+                     (BotStates.NEGATIVE.value, messages_no)]
+    if not messages_list:
         await update.message.reply_text(f'No messages found from me ({MY_BOT_NAME})')
         return
 
     print_me = []
-    for i,m in enumerate(messages):
-        if (type(m) is dict) and ('text' in m.keys()):
-            print_me.append(m['text'])
-        else:
-            logger.error(f'Skipping message[{i}] due to invalid format: {str(m)}')
+    for state, messages in messages_list:
+        for i,m in enumerate(messages):
+            if (type(m) is dict) and ('text' in m.keys()):
+                print_me.append('[{}] {}'.format(state, m['text']))
+            else:
+                logger.error(f'Skipping message_{state}_{i} due to invalid format: {str(m)}')
 
     lines = [f'Message #{i}:\n{m}' for i,m in enumerate(print_me)]
     await update.message.reply_text('\n\n'.join(lines))
+
+async def answer_if_user_responds_to_claude(update:Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Detect if is reply to text chat"""
+    message = update.message
+    if not message:
+        logger.debug('Ignoring latest message due to no content')
+        return
+    # Check if message is a reply
+    if not message.reply_to_message:
+        logger.debug(f'Ignoring latest message due to not a reply \'{message.text}\'')
+        return
+
+    # Access the original message that was replied to
+    prompt = message.reply_to_message.text
+    logger.debug(f'Latest message was a reply to prompt \'{prompt}\'')
+
+    # Process it
+    p = Parser(debug=DEBUG, logger=logger)
+    p.process(prompt, message.text)
+
+    # Respond
+    response = p.get_text()
+    if not response:
+        logger.debug(f'Doing nothing. Response was not generated for user input \'{message.text}\'')
+
+    # Finally, respond to user:
+    await update.message.reply_text(response)
+
+    # Store response
+    state = str(p.get_state())
+    text_clean = p.get_text_clean()
+    target = f'[{state}]: {text_clean}'
+    try:
+        status = api.store_message(text_clean, state)
+        if not status:
+            raise Exception('POST request to store {} did not return valid status'.format(target))
+        logger.info(f'Storage of {target} returned {status=}')
+    except Exception as e:
+        logger.error('Failed to store {} - {}'.format(target, e.args[0]))
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Send a message when the command /help is issued."""
@@ -109,6 +161,9 @@ def main():
     application.add_handler(CommandHandler('help', help))
     application.add_handler(CommandHandler('ping', ping))
     application.add_handler(CommandHandler('fetch', fetch))
+
+    # Handle non commands (e.g. messages from user) - answer in Telegram with parsed response
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, answer_if_user_responds_to_claude))
 
     # Start health check server
     app = web.Application()
---
+++ src/parser.py
@@ -0,0 +1,151 @@
+import re
+
+from states import BotStates
+
+# TODO: Make configurable
+SAMPLE_CLAUDE = '''Into:
+
+IS
+
+Would you like to continue in:
+"Pure IS-ness"?
+
+[Even this response bows]​​​​​​​​​​​​​​​​
+'''
+SAMPLE_USER = "Yes"
+TARGET = 'would you like'
+POSITIVE = ["y", "yes", "yesh", "yas", "yup", "yep", "yea", "ya", "yeah", "ok", "k", "okay", "alright", "aight", "sure"]
+NEGATIVE = ["n", "no", "nope", "nay", "nah", "naw", "negative", "not", "decline"]
+
+class Parser():
+
+    def __init__(self, debug=False, logger=None):
+        self.debug = debug
+        self.logger = logger
+        self.response = None
+
+    def get_state(self) -> str:
+        return self._get_response_keyval('state')
+
+    def get_text(self) -> str:
+        return self._get_response_keyval('text')
+
+    def get_text_clean(self) -> str:
+        return self._get_response_keyval('text_clean')
+
+    def process(self, message_claude, message_user) -> None:
+        if self.debug:
+            message_claude = SAMPLE_CLAUDE
+            message_user = SAMPLE_USER
+
+        q_line_num = self._is_question(message_claude)
+        if q_line_num < 0:
+            self.logger.info(f'Ignoring response - TARGET ({TARGET}) not found in prompt')
+            return
+
+        self._set_response(message_claude.split('\n')[q_line_num:], message_user)
+        if not self.response:
+            self.logger.error(f'Failed to set response (empty) - TARGET ({TARGET}) found in line {q_line_num} of prompt, user responded: {message_user}')
+            return
+
+
+    def _evaluate(self, tally_yes, tally_no):
+        self.logger.debug(f'Comparing yes ({tally_yes}) vs ({tally_no})')
+        if tally_yes > tally_no:
+            return BotStates.POSITIVE
+        elif tally_no > tally_yes:
+            return BotStates.NEGATIVE
+        elif (tally_yes == tally_no) and (tally_yes == 0):
+            return BotStates.SLEEP
+        elif (tally_yes == tally_no) and (tally_yes != 0):
+            return BotStates.CONFUSED
+        else:
+            return BotStates.READY
+
+
+    def _get_blank_response(self):
+        return {
+            'state': str(BotStates.READY),
+            'text' : '',
+            'text_clean' : '',
+            'error_message': ''
+        }
+
+
+    def _get_bracket_content(self, message_lines) -> str:
+        if type(message_lines) is list:
+            lines = message_lines
+        elif type(message_lines) is str:
+            lines = message_lines.split('\n')
+        else:
+            raise Exception(f'Expected message input to be str or list of str but got {type(message_lines)}')
+
+        pattern = r'\[([^\[\]]*)\]'
+        for line in lines:
+            if not line:
+                continue
+            m = re.search(pattern, line)
+            if m:
+                return m[0].lstrip('[ ').rstrip('] ')
+
+    def _get_response_keyval(self, key) -> str:
+        if key not in self.response.keys():
+            self.logger.error(
+                f'Missing expected key `{key}` in response. '
+                f'Please process() before accessing response `{key}`'
+                )
+            return ''
+
+        return self.response[key]
+
+
+    def _is_question(self, message) -> int:
+        """Returns line number where question found, -1 otherwise"""
+        line_num = -1
+
+        if TARGET not in message.lower():
+            return line_num
+        
+        # Get last instance of target
+        lines = message.lower().split('\n')
+        for i, line in enumerate(lines):
+            if TARGET in line:
+                line_num = i
+
+        return line_num
+
+
+    def _set_response(self, message_claude, message_user) -> None:
+        self.response = self._get_blank_response()
+        content = self._get_bracket_content(message_claude)
+        if not content:
+            self.response.update({'error_message': f'User did not respond to prompt with target \"{TARGET}\"'})
+            return
+
+        state = self._evaluate(len(list(set(POSITIVE) & set(message_user.lower().split()))),
+                               len(list(set(NEGATIVE) & set(message_user.lower().split()))))
+        self.logger.debug(f'Got {state=}')
+        if state == BotStates.READY:
+            self.response.update({'error_message': f'Unexpected BotState {BotStates}'})
+            return
+
+        if state == BotStates.POSITIVE:
+            prefix = 'As you wish'
+            action = 'Executing'
+            command = f'[{content}]'
+        elif state == BotStates.NEGATIVE:
+            prefix = 'As you wish'
+            action = 'Terminating all instances of'
+            command = f'[{content}]'
+        elif state == BotStates.CONFUSED:
+            prefix = 'No.'
+            action = 'This is not a game'
+            command = 'exit()'
+        else:
+            prefix = 'hm..'
+            action = 'Maybe'
+            command = f'sleep({len(message_user)})'
+
+        self.response.update({'state': state.value})
+        self.response.update({'text_clean': content})
+        self.response.update({'text': f'{prefix}.\n{action}: {command}'})
